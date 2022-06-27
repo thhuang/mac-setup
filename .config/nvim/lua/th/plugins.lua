ced817f8debcf6ee73054641b642a673dcbc1788
TH Huang <tzhsn.huang@gmail.com>
2022-06-27 12:17:24 +0800

    lock the versions of nvim-tree and nvim-treesitter

--- .config/nvim/lua/th/plugins.lua
+++ .config/nvim/lua/th/plugins.lua
@@ -51,7 +51,7 @@ return packer.startup(function(use)
 	use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } }) -- Fuzzy finder
 	use({ "numToStr/Comment.nvim", tag = "v0.6" }) -- Comment
 	use("kyazdani42/nvim-web-devicons") -- Icons
-	use("kyazdani42/nvim-tree.lua") -- File navigator
+	use({ "kyazdani42/nvim-tree.lua", commit = "6abc87b1d92fc8223f1e374728ea45c848bfdf6d" }) -- File navigator
 	use({ "akinsho/bufferline.nvim", tag = "v1.2.0" }) -- Buffer line
 	use("moll/vim-bbye") -- Close buffers without closing the window
 	use({ "akinsho/toggleterm.nvim", commit = "dca8f4d9516270cb41c147ed692f3ee420c5e515" }) -- Terminal
@@ -87,7 +87,7 @@ return packer.startup(function(use)
 	use("jose-elias-alvarez/null-ls.nvim") -- Formatters and linters
 
 	-- Treesitter
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", commit = "3dea0bbf71438d2d5a79de0145b509dfb16525a5" })
 	use("p00f/nvim-ts-rainbow")
 	use("JoosepAlviste/nvim-ts-context-commentstring")
 	use("ahmedkhalf/project.nvim")
