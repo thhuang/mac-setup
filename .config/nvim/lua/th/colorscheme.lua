7e66c40e45e78c5c911433054888f330edd98686
TH Huang <tzhsn.huang@gmail.com>
2022-09-19 16:33:28 -0500

    update color theme

--- .config/nvim/lua/th/colorscheme.lua
+++ .config/nvim/lua/th/colorscheme.lua
@@ -1,13 +1,10 @@
-local colorscheme = "tokyonight"
+local colorscheme = "tokyonight-night"
 
-vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "terminal", "packer" }
 
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
 end
-
