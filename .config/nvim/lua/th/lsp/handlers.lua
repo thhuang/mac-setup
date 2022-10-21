842adeaad1c8887f422a5830cc0dbfded4e69089
TH Huang <tzhsn.huang@gmail.com>
2022-10-21 10:44:29 -0500

    change resolved_capabilities to server_capabilities

--- .config/nvim/lua/th/lsp/handlers.lua
+++ .config/nvim/lua/th/lsp/handlers.lua
@@ -86,23 +86,23 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "clangd" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "html" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "jsonls" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	lsp_keymaps(bufnr)
